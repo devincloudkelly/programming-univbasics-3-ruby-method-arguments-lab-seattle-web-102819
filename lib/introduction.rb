737efe993352fbf8c54ae3c8da17e0b4c4ca9dee
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-09-19 23:31:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,9 +7,12 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language_optional(name, language = 'Ruby')
+  puts "Hi, my name is #{name} and I am learning to program in #{name}."
+end
 
 
-
+  
 
 
 
